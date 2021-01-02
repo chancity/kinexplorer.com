bae3f86c078a0e289845f136bb5bd9d4f21dc83d
chancity <hawbakerchancey@gmail.com>
2021-01-02 00:07:24 -0800

    removed unused icons

--- app/components/Icons/index.js
+++ app/components/Icons/index.js
@@ -1,12 +1,12 @@
-export {default as AccountIcon} from './AccountIcon';
+/*export {default as AccountIcon} from './AccountIcon';
 export {default as AccountIconDark} from './AccountIconDark';
-export {default as ArrowRight} from './ArrowRight';
-export {default as CaratDownIcon} from './CaratDownIcon';
 export {default as CaratLeftIcon} from './CaratLeftIcon';
 export {default as CaratRightIcon} from './CaratRightIcon';
 export {default as CloseIcon} from './CloseIcon';
 export {default as CloseIconDark} from './CloseIconDark';
+export {default as ModalCloseIcon} from './ModalCloseIcon';*/
+export {default as ArrowRight} from './ArrowRight';
+export {default as CaratDownIcon} from './CaratDownIcon';
 export {default as HamburgerButton} from './HamburgerButton';
-export {default as ModalCloseIcon} from './ModalCloseIcon';
 export {default as SearchCloseIcon} from './SearchCloseIcon';
 export {default as SearchIcon} from './SearchIcon';
