4d6c775d340b9b3c0b97567b8d80134388734a7e
chancity <hawbakerchancey@gmail.com>
2021-01-01 23:51:22 -0800

    moved github icon

--- app/components/Layout/Footer/Footer.js
+++ app/components/Layout/Footer/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import GithubIcon from '../../Icons/GihubIcon';
 import layoutMixin from '../LayoutMixin';
 
 
@@ -8,29 +7,10 @@ const Container = styled.footer`
   ${layoutMixin}
 `;
 
-const Anchor = styled.a`
-  :hover {
-    svg {
-      fill: ${({theme}) => theme.color.interactive.hover}
-    }
-  }
-`
 
-const StyledGithubIcon = styled(GithubIcon)`
-  height: 1.5rem;
-  width: 1.5rem;
-  fill: ${({theme}) => theme.color.interactive.normal}
-`;
 
 const Footer = () => (
   <Container>
-    <Anchor
-      href={'https://github.com/chancity/kinexplorer.com'}
-      target='_blank'
-      rel='noreferrer'
-    >
-      <StyledGithubIcon/>
-    </Anchor>
   </Container>
 );
 
--- app/components/Layout/Header/HeaderBottom.js
+++ app/components/Layout/Header/HeaderBottom.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import layoutMixin from '../LayoutMixin';
 import KinPrice from './KinPrice';
+import GithubIcon from '../../Icons/GihubIcon';
 
 const Container = styled.div`
   background-color: ${({theme}) => theme.color.background.textArea};
@@ -12,15 +13,34 @@ const InnerContainer = styled.div`
   height: 2.625rem;
   padding: 0.5rem 0;
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
 `;
 
+const Anchor = styled.a`
+  :hover {
+    svg {
+      fill: ${({theme}) => theme.color.interactive.hover}
+    }
+  }
+`
 
+const StyledGithubIcon = styled(GithubIcon)`
+  height: 1.5rem;
+  width: 1.5rem;
+  fill: ${({theme}) => theme.color.interactive.normal}
+`;
 
 const HeaderTop = () => {
   return (
     <Container>
       <InnerContainer>
+        <Anchor
+          href={'https://github.com/chancity/kinexplorer.com'}
+          target='_blank'
+          rel='noreferrer'
+        >
+          <StyledGithubIcon/>
+        </Anchor>
         <KinPrice/>
       </InnerContainer>
     </Container>
